258a7d95ab8ccb6edb946a3d3fe82d9f7ace9a73
vyacheslav <you@example.com>
2019-10-17 22:22:06 +0900

    изменение

--- src/vendor/libs/parser.php
+++
@@ -1,8 +0,0 @@
-<?php 
-function Parse($p1, $p2, $p3)
-{
-    $num1 = strpos($p1, $p2);
-    if ($num1 === false) return 0;
-    $num2 = substr($p1, $num1);
-    return strip_tags(substr($num2, 0, strpos($num2, $p3)));
-}
