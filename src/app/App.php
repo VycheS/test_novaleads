13f7906a39b75985f36248b14b181c490ce87d74
vyacheslav <you@example.com>
2019-10-18 22:03:01 +0900

    изменение

--- src/app/App.php
+++ src/app/App.php
@@ -1,6 +1,7 @@
 <?php
 class App
 {
+    private $matches;
     function __construct()
     {
         //echo "start App";
@@ -10,6 +11,12 @@ class App
     {
         $html = file_get_contents($domainName . $route . $params);
         $result = parse($html, '<div class="bg coupon-row"', '<table class="coupon-row-item">', true, true);
-        debug($result);
+        //debug($result);
+        foreach ($result as $cell) {
+            $key = parse($cell, 'data-event-name="', '" data-live');
+            $var = parse($cell, 'data-event-path="', '">');
+            $this->matches[$key] = $var;
+        }
+        debug($this->matches);
     }
 }
