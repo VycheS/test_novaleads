9ba46f03dfa4f6e09d44c2c4280a006430f770df
VycheS <lieber.freund@mail.ru>
2019-10-19 23:47:29 +0900

    правка

--- src/app/App.php
+++ src/app/App.php
@@ -9,20 +9,25 @@ class App
 
     public function start(string $domainName, string $route, string $params)
     {
-        $html = file_get_contents($domainName . $route . $params . '&page=0&pageAction=getPage');
-        $resultAll = parse($html, '<div class=\"bg coupon-row\"', '<table class=\"coupon-row-item\">', true, true);
+        $page  = '&page=';
+        $action = '&pageAction=getPage';
+        $html = file_get_contents($domainName . $route . $params . $page . 0 . $action);
+        $resultAll = parse($html, 'bg coupon-row\"', '<table class=', true, true);
         //debug($resultAll);
-        $html = file_get_contents($domainName . $route . $params . '&page=1&pageAction=getPage');
-        $resultOne = parse($html, '<div class=\"bg coupon-row\"', '<table class=\"coupon-row-item\">', true, true);
-        $resultAll = array_merge($resultAll, $resultOne);
-
+        
+        for ($i = 1; $i <= 3; $i++) {
+            $query = $page . $i . $action;
+            $html .= file_get_contents($domainName . $route . $params . $query);
+            $resultOne = parse($html, 'bg coupon-row\"', '<table class=', true, true);
+            $resultAll += $resultOne;
+        }
 
 
 
         foreach ($resultAll as $cell) {
             $key = parse($cell, 'data-event-name=\"', '\"');
             $var = parse($cell, 'data-event-path=\"', '\">');
-            $this->matches[$key] = $var;
+            $this->matches[$key] = $domainName . '/su/betting/' . $var;
         }
         debug($this->matches);
     }
