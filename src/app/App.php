9407e9149196e8fbd68d6babe4dd4308c15db159
vyacheslav <you@example.com>
2019-10-19 22:00:42 +0900

    правка

--- src/app/App.php
+++ src/app/App.php
@@ -9,12 +9,19 @@ class App
 
     public function start(string $domainName, string $route, string $params)
     {
-        $html = file_get_contents($domainName . $route . $params);
-        $result = parse($html, '<div class="bg coupon-row"', '<table class="coupon-row-item">', true, true);
-        //debug($result);
-        foreach ($result as $cell) {
-            $key = parse($cell, 'data-event-name="', '" data-live');
-            $var = parse($cell, 'data-event-path="', '">');
+        $html = file_get_contents($domainName . $route . $params . '&page=0&pageAction=getPage');
+        $resultAll = parse($html, '<div class=\"bg coupon-row\"', '<table class=\"coupon-row-item\">', true, true);
+        //debug($resultAll);
+        $html = file_get_contents($domainName . $route . $params . '&page=1&pageAction=getPage');
+        $resultOne = parse($html, '<div class=\"bg coupon-row\"', '<table class=\"coupon-row-item\">', true, true);
+        $resultAll = array_merge($resultAll, $resultOne);
+
+
+
+
+        foreach ($resultAll as $cell) {
+            $key = parse($cell, 'data-event-name=\"', '\"');
+            $var = parse($cell, 'data-event-path=\"', '\">');
             $this->matches[$key] = $var;
         }
         debug($this->matches);
